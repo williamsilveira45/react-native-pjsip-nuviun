84b4285bdbb8796e1bc0b247b1c9b8d795e41262
William Silveira <williamlimadasilveira@gmail.com>
2022-06-19 18:35:06 -0300

    fixing url

--- dependencies.sh
+++ dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-DOWNLOAD_LINK="https://drive.google.com/file/d/1vmipYdd1pAQdVhfHel_93FpT1U6_sxZO/view?usp=sharing"
+DOWNLOAD_LINK="https://nuviun.com.br/VialerPJSIP.framework.zip"
 
 cd ios && curl -L $DOWNLOAD_LINK -o dependencies.zip && unzip dependencies.zip && rm dependencies.zip
