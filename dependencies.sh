e7fc41d3e841883f7b51c899b5790d5f628d93d9
William Silveira <williamlimadasilveira@gmail.com>
2022-06-21 18:43:51 -0300

    --silent

--- dependencies.sh
+++ dependencies.sh
@@ -1,6 +1,3 @@
-#!/bin/bash
-set -e
-
 DOWNLOAD_LINK="https://nuviun.com.br/VialerPJSIP.framework.zip"
 
 cd ios && curl --silent -L $DOWNLOAD_LINK -o dependencies.zip && unzip dependencies.zip && rm dependencies.zip
